44f3fd7758beb35e056374ef5c18b0605fca25af
Samim MBT <samimmbt@gmail.com>
2024-06-29 18:06:02 +0330

    Creating entity for users and adding form templates

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,22 +13,55 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     #[Route('/signIn', name: 'app_signIn')]
-    public function signIn(): Response
+    public function signIn(Request $request): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $session = $request->getSession();
+        $logged = $session->get("logged");
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            
+
+            return $this->redirectToRoute('task_success');
+        }
+
         return $this->render('user/signIn.html.twig', [
             'controller_name' => 'UserController',
+            "is_logged" => $logged,
+            'form' => $form->createView()
         ]);
     }
 
-    #[Route('/', name: 'app_main_page')]
-    public function index(Request $request): Response
+    #[Route('/home', name: 'app_main_page')]
+    public function main(Request $request): Response{
+        $session = $request->getSession();
+        $logged = $session->get("logged");
+
+        return $this->render('base.html.twig', ["is_logged" => $logged,]);  
+    }
+
+    #[Route('/', name: 'app_index_page')]
+    public function index(Request $request): RedirectResponse
     {
         $session = $request->getSession();
         $logged = $session->get("logged");
-        if(isset($logged) && !$logged){
-           return $this->redirectToRoute('app_signIn');
-        }else{
-            return $this->render('base.html.twig',['isLogged'=>$logged] );
+
+        if (!isset($logged) && ($logged == false)) {
+
+            return $this->redirectToRoute('app_signIn',
+             ["is_logged" => false]);
+
+        } else if (isset($logged) && ($logged == true)) {
+
+            return $this->redirectToRoute('app_main_page',
+             ["is_logged" => $logged,]);
+
         }
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $age = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getAge(): ?\DateTimeInterface
+    {
+        return $this->age;
+    }
+
+    public function setAge(?\DateTimeInterface $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
