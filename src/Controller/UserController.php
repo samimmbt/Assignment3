93d3740397ae3102928d2c66cc4f3df5614a192c
Samim MBT <samimmbt@gmail.com>
2024-06-29 11:28:25 +0330

    Styling and adding first Page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,14 +1,31 @@
 <?php
-namespace Assignment3\Src\Controller;
+
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class UserController extends AbstractController{
-#[Route('/signin')]
-    public function Sign() : Response
+class UserController extends AbstractController
+{
+    #[Route('/signIn', name: 'app_signIn')]
+    public function signIn(): Response
     {
-        return $this->render('SignIn.html.twing',[]);
+        return $this->render('user/signIn.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/', name: 'app_main_page')]
+    public function index(Request $request): Response
+    {
+        $session = $request->getSession();
+        $logged = $session->get("logged");
+        if(isset($logged) && !$logged){
+           return $this->redirectToRoute('app_signIn');
+        }else{
+            return $this->render('base.html.twig',['isLogged'=>$logged] );
+        }
     }
 }
